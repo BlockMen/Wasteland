d82202fbd89b7c03ef9a1d275cba6754db3bc648
BlockMen <nmuelll@web.de>
2014-04-27 03:17:16 +0200

    Drop items and pick up with "fast" key

--- mods/builtin_item/init.lua
+++ mods/builtin_item/init.lua
@@ -34,16 +34,16 @@ minetest.register_entity(":__builtin:item", {
 			visual = "sprite",
 			textures = {"unknown_item.png"}
 		}
-		if item_texture and item_texture ~= "" then
+		--[[if item_texture and item_texture ~= "" then
 			prop.visual = "sprite"
 			prop.textures = {item_texture}
 			prop.visual_size = {x=0.50, y=0.50}
-		else
+		else]]
 			prop.visual = "wielditem"
 			prop.textures = {itemname}
 			prop.visual_size = {x=0.20, y=0.20}
 			prop.automatic_rotate = math.pi * 0.25
-		end
+		--end
 		self.object:set_properties(prop)
 	end,
 
