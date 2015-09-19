2be7b44211ff9d072ef13bfadddaf5ec0e576c33
BlockMen <nmuelll@web.de>
2015-09-19 11:46:37 +0200

    Cleanup builtin_item mod

--- mods/builtin_item/init.lua
+++ mods/builtin_item/init.lua
@@ -1,18 +1,44 @@
+local lifetime = tonumber(minetest.setting_get("remove_items")) or 300
+
+local function get_flowing_dir(npos)
+	local pos = table.copy(npos)
+		local param2 = minetest.get_node(pos).param2
+		for i, d in ipairs({-1, 1, -1, 1}) do
+			if i < 3 then
+				pos.x = pos.x + d
+			else
+				pos.z = pos.z + d
+			end
+
+			local name = minetest.get_node(pos).name
+			local par2 = minetest.get_node(pos).param2
+			if name == "default:water_flowing" and par2 < param2 then
+				return pos
+			end
+
+			if i < 3 then
+				pos.x = pos.x - d
+			else
+				pos.z = pos.z - d
+			end
+		end
+end
+
 minetest.register_entity(":__builtin:item", {
 	initial_properties = {
 		hp_max = 1,
 		physical = true,
-		collisionbox = {-0.17,-0.17,-0.17, 0.17,0.17,0.17},
-		visual = "sprite",
-		visual_size = {x=0.5, y=0.5},
-		textures = {""},
-		spritediv = {x=1, y=1},
-		initial_sprite_basepos = {x=0, y=0},
+		collisionbox = {-0.17, -0.17, -0.17, 0.17, 0.17, 0.17},
+		visual = "wielditem",
+		visual_size = {x = 0.25, y = 0.25},
+		textures = {"unknown_item.png"},
 		is_visible = false,
 		timer = 0,
+		collide_with_objects = false,
+		automatic_rotate = math.pi * 0.25
 	},
-	
-	itemstring = '',
+
+	itemstring = "",
 	physical_state = true,
 
 	set_item = function(self, itemstring)
@@ -25,25 +51,16 @@ minetest.register_entity(":__builtin:item", {
 		end
 		local item_texture = nil
 		local item_type = ""
-		if minetest.registered_items[itemname] then
-			item_texture = minetest.registered_items[itemname].inventory_image
-			item_type = minetest.registered_items[itemname].type
+		local def = minetest.registered_items[itemname]
+		if def then
+			item_texture = def.inventory_image
+			item_type = def.type
 		end
-		prop = {
+		local prop = {
 			is_visible = true,
-			visual = "sprite",
-			textures = {"unknown_item.png"}
+			visual = "wielditem",
+			textures = {itemname}
 		}
-		--[[if item_texture and item_texture ~= "" then
-			prop.visual = "sprite"
-			prop.textures = {item_texture}
-			prop.visual_size = {x=0.50, y=0.50}
-		else]]
-			prop.visual = "wielditem"
-			prop.textures = {itemname}
-			prop.visual_size = {x=0.20, y=0.20}
-			prop.automatic_rotate = math.pi * 0.25
-		--end
 		self.object:set_properties(prop)
 	end,
 
@@ -66,106 +83,76 @@ minetest.register_entity(":__builtin:item", {
 				if not self.timer then
 					self.timer = 0
 				end
-				self.timer = self.timer+dtime_s
+				self.timer = self.timer + dtime_s
 			end
 		else
 			self.itemstring = staticdata
 		end
-		self.object:set_armor_groups({immortal=1})
-		self.object:setvelocity({x=0, y=2, z=0})
-		self.object:setacceleration({x=0, y=-10, z=0})
+		self.object:set_armor_groups({immortal = 1})
+		self.object:setvelocity({x = 0, y = 2, z = 0})
+		self.object:setacceleration({x = 0, y = -10, z = 0})
 		self:set_item(self.itemstring)
 	end,
-	
+
 	on_step = function(self, dtime)
-		local time = minetest.setting_get("remove_items")
-		if not time then
-			time = 300
-		end
 		if not self.timer then
 			self.timer = 0
 		end
 		self.timer = self.timer + dtime
-		time = tonumber(time)
-		if time ~= 0 and (self.timer > time) then
+
+		if lifetime ~= 0 and (self.timer > lifetime) then
 			self.object:remove()
 		end
-		
-		local p = self.object:getpos()
-		
-		local name = minetest.env:get_node(p).name
-		if name == "default:lava_flowing" or name == "default:lava_source" then
-			minetest.sound_play("builtin_item_lava", {pos=self.object:getpos()})
+
+		local pos = self.object:getpos()
+		local nn = minetest.get_node(pos).name
+		local ndef = minetest.registered_nodes[nn]
+
+		if nn == "default:lava_flowing" or nn == "default:lava_source" then
+			minetest.sound_play("builtin_item_lava", {pos = pos})
+			self.timer = lifetime + 1
 			self.object:remove()
 			return
 		end
-		
-		if minetest.registered_nodes[name].liquidtype == "flowing" then
-			get_flowing_dir = function(self)
-				local pos = self.object:getpos()
-				local param2 = minetest.env:get_node(pos).param2
-				for i,d in ipairs({-1, 1, -1, 1}) do
-					if i<3 then
-						pos.x = pos.x+d
-					else
-						pos.z = pos.z+d
-					end
-					
-					local name = minetest.env:get_node(pos).name
-					local par2 = minetest.env:get_node(pos).param2
-					if name == "default:water_flowing" and par2 < param2 then
-						return pos
-					end
-					
-					if i<3 then
-						pos.x = pos.x-d
-					else
-						pos.z = pos.z-d
-					end
-				end
-			end
-			
-			local vec = get_flowing_dir(self)
+
+		if ndef.liquidtype == "flowing" then
+			local vec = get_flowing_dir(pos)
 			if vec then
 				local v = self.object:getvelocity()
-				if vec and vec.x-p.x > 0 then
-					self.object:setvelocity({x=0.5,y=v.y,z=0})
-				elseif vec and vec.x-p.x < 0 then
-					self.object:setvelocity({x=-0.5,y=v.y,z=0})
-				elseif vec and vec.z-p.z > 0 then
-					self.object:setvelocity({x=0,y=v.y,z=0.5})
-				elseif vec and vec.z-p.z < 0 then
-					self.object:setvelocity({x=0,y=v.y,z=-0.5})
+				if vec and vec.x - pos.x > 0 then
+					self.object:setvelocity({x = 0.5, y = v.y, z = 0})
+				elseif vec and vec.x - pos.x < 0 then
+					self.object:setvelocity({x = -0.5, y = v.y, z = 0})
+				elseif vec and vec.z - pos.z > 0 then
+					self.object:setvelocity({x = 0, y = v.y, z = 0.5})
+				elseif vec and vec.z - pos.z < 0 then
+					self.object:setvelocity({x = 0,y = v.y, z = -0.5})
 				end
-				self.object:setacceleration({x=0, y=-10, z=0})
+
+				self.object:setacceleration({x = 0, y = -10, z = 0})
 				self.physical_state = true
-				self.object:set_properties({
-					physical = true
-				})
+				self.object:set_properties({physical = true})
 				return
 			end
 		end
-		
-		p.y = p.y - 0.3
-		local nn = minetest.env:get_node(p).name
+
+		pos.y = pos.y - 0.3
+		nn = minetest.get_node_or_nil(pos).name
+		ndef = minetest.registered_nodes[nn]
 		-- If node is not registered or node is walkably solid
-		if not minetest.registered_nodes[nn] or minetest.registered_nodes[nn].walkable then
+		if not ndef or ndef.walkable then
 			if self.physical_state then
-				self.object:setvelocity({x=0,y=0,z=0})
-				self.object:setacceleration({x=0, y=0, z=0})
+				self.object:setvelocity({x = 0, y = 0, z = 0})
+				self.object:setacceleration({x = 0, y = 0, z = 0})
 				self.physical_state = false
-				self.object:set_properties({
-					physical = false
-				})
+				self.object:set_properties({physical = false})
 			end
 		else
 			if not self.physical_state then
-				self.object:setvelocity({x=0,y=0,z=0})
-				self.object:setacceleration({x=0, y=-10, z=0})
+				self.object:setvelocity({x = 0, y = 0, z = 0})
+				self.object:setacceleration({x = 0, y =- 10, z = 0})
 				self.physical_state = true
-				self.object:set_properties({
-					physical = true
-				})
+				self.object:set_properties({physical = true})
 			end
 		end
 	end,
@@ -181,7 +168,3 @@ minetest.register_entity(":__builtin:item", {
 		self.object:remove()
 	end,
 })
-
-if minetest.setting_get("log_mods") then
-	minetest.log("action", "builtin_item loaded")
-end
